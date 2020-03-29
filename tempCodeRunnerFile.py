07ec1a3b0acfefae66c58ae12c0466667f766f76
jwong2267 <yukwa.wong@baruchmail.cuny.edu>
2020-03-29 15:51:42 -0400

    logicrecursion2

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,14 +1,9 @@
 while True:
     try:
-        # Note: Python 2.x users should use raw_input, the equivalent of 3.x's input
         user_int = int(input("Please choose an integer: "))
     except :
-        # print("Sorry, I didn't understand that.")
-        # #better try again... Return to the start of the loop
         continue
     else:
-        #age was successfully parsed!
-        #we're ready to exit the loop.
         break
 if user_int % 2 == 0:
     print("even")
